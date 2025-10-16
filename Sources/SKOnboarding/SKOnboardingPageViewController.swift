dc26c0452db3e2bc28ab5694120c9e8ae97812e8
sametkorkmaaz <smt.krkmz.2002@gmail.com>
2025-10-17 01:10:15 +0300

    bug fix

--- Sources/SKOnboarding/SKOnboardingPageViewController.swift
+++ Sources/SKOnboarding/SKOnboardingPageViewController.swift
@@ -89,8 +89,8 @@ class SKOnboardingPageViewController: UIViewController {
         NSLayoutConstraint.activate([
             stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32),
             stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -32),
-            stackView.topAnchor.constraint(greaterThanOrEqualTo: view.topAnchor, constant: view.frame.size.height * 0.1),
-            stackView.bottomAnchor.constraint(lessThanOrEqualTo: view.bottomAnchor, constant: -view.frame.size.height * 0.4),
+            stackView.topAnchor.constraint(greaterThanOrEqualTo: view.topAnchor, constant: view.frame.size.height * 0.01),
+            stackView.bottomAnchor.constraint(lessThanOrEqualTo: view.bottomAnchor, constant: -view.frame.size.height * 0.04),
             
             imageContainerView.widthAnchor.constraint(equalTo: imageContainerView.heightAnchor),
             imageContainerView.widthAnchor.constraint(lessThanOrEqualTo: stackView.widthAnchor),
