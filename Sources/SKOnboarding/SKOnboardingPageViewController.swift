b39fc5a8d03e910edf3eee41ffa268c5e8361cb2
sametkorkmaaz <smt.krkmz.2002@gmail.com>
2025-10-17 01:44:15 +0300

    bug fix

--- Sources/SKOnboarding/SKOnboardingPageViewController.swift
+++ Sources/SKOnboarding/SKOnboardingPageViewController.swift
@@ -30,7 +30,7 @@ class SKOnboardingPageViewController: UIViewController {
 
     private let imageView: UIImageView = {
         let imageView = UIImageView()
-        imageView.contentMode = .scaleAspectFill
+        imageView.contentMode = .scaleToFill
         imageView.clipsToBounds = true
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
