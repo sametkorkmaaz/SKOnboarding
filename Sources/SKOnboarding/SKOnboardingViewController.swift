9809ccf805e24b2d1ec7cff99812f2291879afb5
sametkorkmaaz <smt.krkmz.2002@gmail.com>
2025-10-19 02:21:41 +0300

    set hidden

--- Sources/SKOnboarding/SKOnboardingViewController.swift
+++ Sources/SKOnboarding/SKOnboardingViewController.swift
@@ -169,7 +169,7 @@ class SKOnboardingViewController: UIViewController {
         pageControl.pageIndicatorTintColor = currentPage.mainColor.withAlphaComponent(0.3)
         
         let buttonTitle = isLastPage ? "DONE" : "NEXT"
-        skipButton.isHidden = isLastPage
+        skipButton.setHidden(isLastPage, animated: true)
         nextButton.setTitle(buttonTitle, for: .normal)
     }
 }
